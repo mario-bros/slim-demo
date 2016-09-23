70b71e6d08af852e79c63435181d0be14d2d65fe
Dominik Zogg <dominik.zogg@gmail.com>
2016-09-23 14:17:12 +0200

    move Doctrine\Common\Annotations\AnnotationRegistry to the providers.php

--- app/autoload.php
+++ app/autoload.php
@@ -1,11 +1,7 @@
 <?php
 
-use Doctrine\Common\Annotations\AnnotationRegistry;
-
 if (!$loader = @include __DIR__.'/../vendor/autoload.php') {
     die('curl -s http://getcomposer.org/installer | php; php composer.phar install');
 }
-
-AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
 return $loader;
--- app/providers.php
+++ app/providers.php
@@ -1,9 +1,12 @@
 <?php
 
 use Dflydev\Provider\DoctrineOrm\DoctrineOrmServiceProvider;
+use Doctrine\Common\Annotations\AnnotationRegistry;
 use Saxulum\Console\Provider\ConsoleProvider;
 use Saxulum\DoctrineOrmManagerRegistry\Provider\DoctrineOrmManagerRegistryProvider;
 use Silex\Provider\DoctrineServiceProvider;
+
+AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
 $container->register(new ConsoleProvider());
 $container->register(new DoctrineServiceProvider());
